7f3d8e53a2140a4cf92ce1aea3ad7bdb26a38dd2
yi <297254601@qq.com>
2022-09-26 22:27:35 +0800

    second commit

--- src/git_test/GitTest.java
+++ src/git_test/GitTest.java
@@ -7,5 +7,7 @@ package git_test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 }
