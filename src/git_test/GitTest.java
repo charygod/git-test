abae9835a20c3694ba9e63ed9317f1173cb97682
yi <297254601@qq.com>
2022-09-26 22:26:53 +0800

    first commit

---
+++ src/git_test/GitTest.java
@@ -0,0 +1,11 @@
+package git_test;
+
+/**
+ * @autuor shkstart
+ * @create 2022-09-26 22:09-
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
